98de74f73c8a60b27147636a1c22767bb5bd0598
Ariane Ueti <arianeueti@gmail.com>
2021-06-23 18:23:28 -0300

    Exercício #1 - Criando 5 programas para operações aritméticas

---
+++ exercises/exercises/introducao-a-javascript-e-logica-de-programacao_1/script1.js
@@ -0,0 +1,22 @@
+let a = 1;
+let b = 2;
+
+// adição
+let soma = a + b;
+console.log(soma);
+
+// subtração
+let sub = a - b;
+console.log(sub);
+
+// multiplicação
+let mult = a * b;
+console.log(mult)
+
+// divisão
+let div = a / b;
+console.log(div)
+
+// módulo
+let rest = a % b;
+console.log(rest)
