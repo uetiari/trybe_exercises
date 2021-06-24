bbc3298509c841af73be77ce2b97f9bb4368cf11
Ariane Ueti <arianeueti@gmail.com>
2021-06-24 18:42:46 -0300

    Exercício #1 - Percorrendo Array com console.log

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script1.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+console.log(numbers[0]);
+console.log(numbers[1]);
+console.log(numbers[2]);
+console.log(numbers[3]);
+console.log(numbers[4]);
+console.log(numbers[5]);
+console.log(numbers[6]);
+console.log(numbers[7]);
+console.log(numbers[8]);
+console.log(numbers[9]);
