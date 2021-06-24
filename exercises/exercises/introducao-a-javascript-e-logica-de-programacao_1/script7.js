8b19271848288fdc3a106808abe366a9c56f768a
Ariane Ueti <arianeueti@gmail.com>
2021-06-23 21:55:05 -0300

    Exercício #7 - Notas

---
+++ exercises/exercises/introducao-a-javascript-e-logica-de-programacao_1/script7.js
@@ -0,0 +1,17 @@
+let nota = 41;
+
+if ( nota < 0 || nota > 100) {
+  console.log("Error");
+} else if( nota >= 90){
+  console.log("A");
+} else if (nota >= 80){
+  console.log("B");
+} else if (nota >= 70) {
+  console.log("C");
+} else if(nota >= 60){
+  console.log("D");
+} else if (nota >= 50){
+  console.log("E");
+} else if(nota < 50){
+  console.log("F");
+};
