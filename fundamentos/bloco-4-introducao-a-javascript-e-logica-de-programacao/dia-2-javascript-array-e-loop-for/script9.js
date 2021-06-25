0f0ba193c059c6f68d93b474f17cdd8980ae2b78
Ariane Ueti <arianeueti@gmail.com>
2021-06-24 22:52:04 -0300

    Exercício #8 - Array até 25

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script9.js
@@ -0,0 +1,9 @@
+let list = [];
+
+for (let index = 0; index <= 25; index += 1 ){
+  list.push(index);
+}
+
+for(let index = 0; index < list.length; index += 1 ){
+  console.log(list[index] / 2);
+};
