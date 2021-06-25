d1b3b9b9c3d1c1233291056c7daa968a8ce08392
Ariane Ueti <arianeueti@gmail.com>
2021-06-24 21:10:23 -0300

    Exercício 2 - Soma de todos no Array

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script2.js
@@ -0,0 +1,7 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let results = 0;
+for (let index = 0; index < numbers.length; index += 1 ){
+  results += numbers[index];
+}
+console.log(results)
