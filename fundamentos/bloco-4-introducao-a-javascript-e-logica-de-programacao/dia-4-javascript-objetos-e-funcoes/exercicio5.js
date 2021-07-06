fa19349309fbf4a9f7b279c11157c79ee373c8a4
Ariane Ueti <42356440+uetiari@users.noreply.github.com>
2021-07-06 19:26:41 -0300

    Exercício #5

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio5.js
@@ -0,0 +1,21 @@
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+};
+
+info['recorrentes'] = 'sim';
+info2['recorrentes'] = 'sim';
+
+for (let key in info) {
+  console.log(info[key]);
+}
+for (let key in info2){
+ console.log(info2[key]);
+}
+
