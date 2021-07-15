bf98a2485dea0c1d197ae2718c34eb419b37d859
Ariane Ueti <arianeueti@gmail.com>
2021-07-15 18:48:41 -0300

    Exercício 3 de prática

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/pratica_script1.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/pratica_script1.js
@@ -9,3 +9,6 @@ let player = {
 }
 
 console.log('A jogadora '+ player.name +' '+ player.lastName +' tem '+ player.age +' anos de idade.')
+
+player['bestInTheWorld'] =  [2006, 2007, 2008, 2009, 2010, 2018];
+console.log(player);
