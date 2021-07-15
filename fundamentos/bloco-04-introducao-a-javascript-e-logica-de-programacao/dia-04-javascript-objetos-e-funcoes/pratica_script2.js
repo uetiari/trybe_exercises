2d944d0b0b99c8b166f4bffdbe5bd2b281cd80fd
Ariane Ueti <arianeueti@gmail.com>
2021-07-15 19:12:02 -0300

    Exercício 6 de prática

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/pratica_script2.js
@@ -0,0 +1,37 @@
+let cars = ['Saab', 'Volvo', 'BMW'];
+
+for (let index in cars) {
+  console.log(cars[index]);
+}
+
+let car = {
+  type: 'Fiat',
+  model: '500',
+  color: 'white',
+};
+
+for (let index in car) {
+  console.log(index, car[index]);
+}
+
+let food = ['hamburguer', 'bife', 'acarajé'];
+for (let position in food) {
+  console.log(position);
+};
+//resultado: 0, 1, 2;
+
+let food = ['hamburguer', 'bife', 'acarajé'];
+for (let value of food) {
+  console.log(value);
+};
+//resultado: hamburguer, bife, acarajé;
+
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge' 
+};
+for (let key in names) {
+  console.log('Olá '+ names[key]);
+}
+//resultado: Olá João; Olá Maria; Olá Jorge
