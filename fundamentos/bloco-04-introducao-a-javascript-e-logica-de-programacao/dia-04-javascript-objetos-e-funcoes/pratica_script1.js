ad7f99e65b26e6f9c85ac7909ef1c06ab584fd76
Ariane Ueti <arianeueti@gmail.com>
2021-07-15 18:57:40 -0300

    Exercício 5 de prática

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/pratica_script1.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/pratica_script1.js
@@ -7,11 +7,10 @@ let player = {
     silver: 3
   },
 }
+player['bestInTheWorld'] =  [2006, 2007, 2008, 2009, 2010, 2018];
 
 console.log('A jogadora '+ player.name +' '+ player.lastName +' tem '+ player.age +' anos de idade.')
 
-player['bestInTheWorld'] =  [2006, 2007, 2008, 2009, 2010, 2018];
 console.log('A jogadora '+ player.name +' '+ player.lastName +' foi eleita a melhor do mundo por '+ player.bestInTheWorld.length +' vezes.');
 
-
-// console.log('A jogadora possui '+ player.medals.golden +' medalhas de ouro e '+ player.medals.silver +' medalhas de prata.')
+console.log('A jogadora possui '+ player.medals.golden +' medalhas de ouro e '+ player.medals.silver +' medalhas de prata!')
