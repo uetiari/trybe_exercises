32d8f3972b407464501142d52059472513d550e4
Ariane Ueti <arianeueti@gmail.com>
2021-07-15 20:24:25 -0300

    Exercício 1

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script-exercicio01-a-05.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script-exercicio01-a-05.js
@@ -3,19 +3,19 @@ let info = {
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
-let info2 = {
-  personagem: 'Tio Patinhas',
-  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-  nota: 'O último MacPatinhas',
-};
+// let info2 = {
+//   personagem: 'Tio Patinhas',
+//   origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//   nota: 'O último MacPatinhas',
+// };
 
-info['recorrentes'] = 'sim';
-info2['recorrentes'] = 'sim';
+// info['recorrentes'] = 'sim';
+// info2['recorrentes'] = 'sim';
 
 for (let key in info) {
-  console.log(info[key]);
+  console.log('Bem vinda, '+ info.personagem);
 }
-for (let key in info2){
- console.log(info2[key]);
-}
+// for (let key in info2){
+//  console.log(info2[key]);
+// }
 
