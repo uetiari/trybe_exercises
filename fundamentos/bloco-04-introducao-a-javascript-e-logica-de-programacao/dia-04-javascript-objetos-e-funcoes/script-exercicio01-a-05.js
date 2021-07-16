27e282b574dcc6276f454b0bd8cca860a255f548
Ariane Ueti <arianeueti@gmail.com>
2021-07-15 21:03:44 -0300

    Exercício 2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script-exercicio01-a-05.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script-exercicio01-a-05.js
@@ -3,18 +3,19 @@ let info = {
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
+
+for (let key in info) {
+  console.log('Bem vinda, '+ info.personagem);
+}
+info['recorrentes'] = 'Sim';
+console.log(info);
+
 // let info2 = {
 //   personagem: 'Tio Patinhas',
 //   origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
 //   nota: 'O último MacPatinhas',
 // };
-
-// info['recorrentes'] = 'sim';
 // info2['recorrentes'] = 'sim';
-
-for (let key in info) {
-  console.log('Bem vinda, '+ info.personagem);
-}
 // for (let key in info2){
 //  console.log(info2[key]);
 // }
