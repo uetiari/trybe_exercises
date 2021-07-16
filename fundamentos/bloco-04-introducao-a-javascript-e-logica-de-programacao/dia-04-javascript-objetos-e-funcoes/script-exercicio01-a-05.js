a5b5fe3ee0c0186c62b8429339766b2dd81055b5
Ariane Ueti <arianeueti@gmail.com>
2021-07-15 21:13:14 -0300

    Exercício 5

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script-exercicio01-a-05.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script-exercicio01-a-05.js
@@ -7,8 +7,9 @@ let info = {
 for (let key in info) {
   console.log('Bem vinda, '+ info.personagem);
 }
-info['recorrentes'] = 'Sim';
+info['recorrente'] = 'Sim';
 console.log(info);
+
 for (let key in info) {
   console.log(key);
 }
@@ -16,14 +17,18 @@ for (let key in info) {
   console.log(info[key]);
 }
 
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+};
 
-// let info2 = {
-//   personagem: 'Tio Patinhas',
-//   origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-//   nota: 'O último MacPatinhas',
-// };
-// info2['recorrentes'] = 'sim';
-// for (let key in info2){
-//  console.log(info2[key]);
-// }
-
+for (let key in info) {
+  if ( key === 'recorrente' && info[key] === 'Sim' && info2[key] === 'Sim'
+  ) {
+    console.log('Ambos recorrentes');
+  } else {
+    console.log(info[key] + ' e ' + info2[key]);
+  }
+}
