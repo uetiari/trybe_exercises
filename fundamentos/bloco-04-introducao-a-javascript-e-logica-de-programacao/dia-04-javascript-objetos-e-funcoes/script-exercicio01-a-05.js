856d4fda625ba8804d7aa7447a0436b1453a512b
Ariane Ueti <arianeueti@gmail.com>
2021-07-18 14:34:44 -0300

    Exercício 01

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script-exercicio01-a-05.js
@@ -0,0 +1,9 @@
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+for (let key in info) {
+  console.log('Bem vinda, '+ info.personagem);
+}
