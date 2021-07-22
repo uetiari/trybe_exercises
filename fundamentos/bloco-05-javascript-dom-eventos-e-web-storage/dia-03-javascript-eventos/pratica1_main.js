ea6d5fdedef1c6c67d94c12feff97d452dacc21f
Ariane Ueti <arianeueti@gmail.com>
2021-07-21 21:41:33 -0300

    Prática 5 parte II

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/pratica1_main.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/pratica1_main.js
@@ -32,12 +32,16 @@ function changeInput(event) {
 function redirectPage(event) {
   window.location.replace('https://uetiari.github.io/');
 }
- myWebpage.addEventListener('dblclick', redirectPage());
+ myWebpage.addEventListener('dblclick', redirectPage);
 
  /*
- Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-a cor do mesmo;
+ 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere a cor do mesmo;*/
+function changeColor(event) {
+  event.target.style.color = 'blue';
+}
+ myWebpage.addEventListener('mouseover', changeColor);
 
+ /*
 Segue abaixo um exemplo do uso de event.target:
 */
 
