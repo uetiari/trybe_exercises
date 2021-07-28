ec2c8607638f326a10bd5196168083ce0476ddad
Ariane Ueti <arianeueti@gmail.com>
2021-07-28 14:44:34 -0300

    Exemplo de Higher Order Functions

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/exemplo1.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/exemplo1.js
@@ -5,11 +5,13 @@ function sum (number1, number2) {
 const sumVariable = sum;
 
 console.log(sumVariable);
-//  [Function: sum]
+// Resultado esperado [Function: sum]
 
+// refactor
 const sum = (number1, number2) => {
   return number1 + number2;
 };
+// Resultado esperado [Function: sum]
 
 // Passar funções como argumento para outras funções:
 const sayHello = () => {
