4e7db6d847aef81e1f1cea6c942f5433fc8035e2
Ariane Ueti <arianeueti@gmail.com>
2021-07-29 19:06:11 -0300

    Prática 2

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/pratica2.js
@@ -0,0 +1,13 @@
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = (number) => {
+  // Adiciona seu código aqui
+  if (number % 3 === 0 && number % 5 === 0) {
+    return true;
+  }
+  return false;
+}
+
+const isDivisible = numbers.find(findDivisibleBy3And5);
+
+console.log(findDivisibleBy3And5(11));
