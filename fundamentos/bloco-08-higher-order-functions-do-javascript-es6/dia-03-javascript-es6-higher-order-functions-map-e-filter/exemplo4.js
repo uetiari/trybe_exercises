45737af689c05421825233e12b9500b0c0313afb
Ariane Ueti <arianeueti@gmail.com>
2021-07-30 15:22:56 -0300

    Exercícios de exemplo

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/exemplo4.js
@@ -0,0 +1,24 @@
+const persons = [
+  { firstName: 'Maria', lastName: 'Ferreira' },
+  { firstName: 'João', lastName: 'Silva' },
+  { firstName: 'Antonio', lastName: 'Cabral' },
+];
+
+const fullNames = [];
+
+for (let index = 0; index < persons.length; index += 1) {
+  fullNames.push(`${persons[index].firstName} ${persons[index].lastName}`);
+}
+
+console.log(fullNames);
+
+// usando a função map
+const persons = [
+  { firstName: 'Maria', lastName: 'Ferreira' },
+  { firstName: 'João', lastName: 'Silva' },
+  { firstName: 'Antonio', lastName: 'Cabral' },
+];
+
+const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
+
+console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/exemplo7.js
@@ -0,0 +1,11 @@
+const numeros = [1, 2, 3, 4, 5, 6];
+console.log(numeros.map((numero) => numero * 2)); 
+// Retorno: [2, 4, 6, 8, 10, 12]
+
+const paresMenoresQueCinco = [];
+numeros.forEach((numero) => {
+  if (numero < 5 && numero % 2 === 0) {
+    paresMenoresQueCinco.push(numero);
+  }
+});
+console.log(paresMenoresQueCinco); // Retorno: [2, 4]
