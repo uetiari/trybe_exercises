b81648d2cb8912376e722f6d01dd25290a80b10b
Ariane Ueti <arianeueti@gmail.com>
2021-07-30 18:36:42 -0300

    Des comentado um Teste

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/testes1.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/testes1.js
@@ -33,10 +33,10 @@ function add(a, b) {
   return a + b;
 }
 
-const expected = add(2, 2);
+const expected = add(1, 2);
 
 assert.ok(expected === 3, 'Um mais dois é igual a três'); // Checa se o primeiro argumento é verdadeiro
-// assert.strictEqual(expected, 3, 'Um mais dois é igual a três'); // Checa se o primeiro e segundo argumentos são iguais em valor e tipo (===)
+assert.strictEqual(expected, 3, 'Um mais dois é igual a três'); // Checa se o primeiro e segundo argumentos são iguais em valor e tipo (===)
 // assert.notStrictEqual(expected, 4, 'Um mais dois é igual a três (e não quatro!)'); // Checa se o primeiro e segundo argumentos são diferentes (!==)
 
 //função 5 para testes
