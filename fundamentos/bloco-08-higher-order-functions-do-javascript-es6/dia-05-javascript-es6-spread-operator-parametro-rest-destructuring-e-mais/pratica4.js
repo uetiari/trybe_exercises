798dc5aa9931868369e274bf3ebc9d5eea347b13
Ariane Ueti <arianeueti@gmail.com>
2021-08-03 18:15:21 -0300

    Adicionado explicação sobre vírgulas antes do Spread

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/pratica4.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/pratica4.js
@@ -1,9 +1,10 @@
 let numerosPares = [1, 3, 5, 6, 8, 10, 12];
 
-console.log(numerosPares); // [6, 8, 10, 12];
+// console.log(numerosPares); // [6, 8, 10, 12];
 
 // Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
 
 [,,, ...numerosPares] = numerosPares;
+//as vírgulas são usadas para ignorar as 3 primeiras posições pois não são pares!
 
 console.log(numerosPares); // [6, 8, 10, 12];
