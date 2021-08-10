382eec36c80e7256dbcfc540de4759a225aa86b0
Ariane Ueti <arianeueti@gmail.com>
2021-08-10 19:10:28 -0300

    Exercício 2 - Exercícios Course

--- fundamentos/bloco-09-javascript-assincrono-e-promises/dia-01-javascript-assincrono-e-callbacks/exe5.js
+++ fundamentos/bloco-09-javascript-assincrono-e-promises/dia-01-javascript-assincrono-e-callbacks/exe5.js
@@ -32,3 +32,36 @@ console.log(planetDistanceFromSun(jupiter)); // C
 // Venus is 108200000 kilometers apart from the Sun
 // Jupiter is 778500000 kilometers apart from the Sun
 
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+const mars = {
+  name: "Mars",
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const venus = {
+  name: "Venus",
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const jupiter = {
+  name: "Jupiter",
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: "kilometers",
+  },
+};
+
+console.log(planetDistanceFromSun(mars)); // A
+setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
+// Mars is 227900000 kilometers apart from the Sun
+// Jupiter is 778500000 kilometers apart from the Sun
+// Venus is 108200000 kilometers apart from the Sun
