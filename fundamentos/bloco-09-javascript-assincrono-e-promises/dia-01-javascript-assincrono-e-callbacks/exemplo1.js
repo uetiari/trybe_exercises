ee5cee7703dc430515c97132672a7078a46f9b9c
Ariane Ueti <arianeueti@gmail.com>
2021-08-10 15:31:29 -0300

    Exemplos de Operações Assíncronas

---
+++ fundamentos/bloco-09-javascript-assincrono-e-promises/dia-01-javascript-assincrono-e-callbacks/exemplo1.js
@@ -0,0 +1,18 @@
+// console.log('1 - Receber roda');
+// console.log('2 - Encaixar parafusos');
+// console.log('3 - Fixar roda no carro');
+
+// console.log('1 - Comprar parafusos');
+// console.log('2 - Adicionar ao estoque');
+// console.log('3 - Receber roda');
+// console.log('4 - Encaixar parafusos');
+// console.log('5 - Fixar roda no carro');
+
+setTimeout(() => {
+  console.log('Comprar parafusos') // Comprar parafusos
+  console.log('Adicionar ao estoque') // Adicionar ao estoque
+}, 2000);
+
+console.log('1 - Receber roda'); // 1 - Receber roda
+console.log('2 - Encaixar parafusos'); // 2 - Encaixar parafusos
+console.log('3 - Fixar roda no carro'); // 3 - Fixar roda no carro
