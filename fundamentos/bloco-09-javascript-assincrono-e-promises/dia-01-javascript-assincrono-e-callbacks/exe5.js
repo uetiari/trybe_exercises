6059f01a220331a9638b46d48d6482bd91e78389
Ariane Ueti <arianeueti@gmail.com>
2021-08-10 21:58:43 -0300

    Exercício 4 - Exercícios Course

--- fundamentos/bloco-09-javascript-assincrono-e-promises/dia-01-javascript-assincrono-e-callbacks/exe5.js
+++ fundamentos/bloco-09-javascript-assincrono-e-promises/dia-01-javascript-assincrono-e-callbacks/exe5.js
@@ -78,3 +78,18 @@ const getPlanet = () => {
 };
 getPlanet();
 // imprime Marte depois de 4 segundos
+
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+const sendMarsTemperature = () => {
+  const atualtemp = getMarsTemperature();
+  setTimeout(() => console.log(`Mars temperature is: ${atualtemp} degree Celsius`), messageDelay());
+}; 
+// imprime "Mars temperature is: 20 degree Celsius", por exemplo
+console.log(sendMarsTemperature());
