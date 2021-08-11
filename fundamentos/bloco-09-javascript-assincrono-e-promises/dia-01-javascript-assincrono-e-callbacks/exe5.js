90a35dff73d0b115862a3b8c117935511a8e5b6d
Ariane Ueti <arianeueti@gmail.com>
2021-08-10 22:27:56 -0300

    Exercício 6 - Exercícios Course

--- fundamentos/bloco-09-javascript-assincrono-e-promises/dia-01-javascript-assincrono-e-callbacks/exe5.js
+++ fundamentos/bloco-09-javascript-assincrono-e-promises/dia-01-javascript-assincrono-e-callbacks/exe5.js
@@ -32,6 +32,7 @@ console.log(planetDistanceFromSun(jupiter)); // C
 // Venus is 108200000 kilometers apart from the Sun
 // Jupiter is 778500000 kilometers apart from the Sun
 
+// ----------------------------------
 const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
   `${name} is ${value} ${measurementUnit} apart from the Sun`;
 
@@ -66,6 +67,7 @@ setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
 // Jupiter is 778500000 kilometers apart from the Sun
 // Venus is 108200000 kilometers apart from the Sun
 
+// ----------------------------------
 const getPlanet = () => {
   const mars = {
     name: "Mars",
@@ -79,6 +81,7 @@ const getPlanet = () => {
 getPlanet();
 // imprime Marte depois de 4 segundos
 
+// --------------------------------------
 const messageDelay = () => Math.floor(Math.random() * 5000);
 
 const getMarsTemperature = () => {
@@ -95,6 +98,7 @@ const sendMarsTemperature = () => {
 // console.log(sendMarsTemperature());
 sendMarsTemperature();
 
+// ---------------------------------
 const messageDelay = () => Math.floor(Math.random() * 5000);
 
 const getMarsTemperature = () => {
@@ -117,3 +121,41 @@ const sendMarsTemperature = (onSuccess) => {
 }
 sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
 sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
+
+
+// ------------------------------------
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+}
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+const greet = (temperature) =>
+  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+const handleError = (errorReason) =>
+  console.log(`Error getting temperature: ${errorReason}`);
+
+// definição da função sendMarsTemperature...
+const sendMarsTemperature = (onSuccess, onError) => {
+  const atualtemp = getMarsTemperature();
+  const msgSent = Math.random() < 0.6; // usa 0.6 considerando 100% sendo 1, então 0.6 significa 60%
+  setTimeout(() => {
+    if (msgSent) {
+      onSuccess(atualtemp)
+    } else {
+      onError('Sem internet');
+    }
+  }, messageDelay());
+}
+// imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(temperatureInFahrenheit, handleError);
+
+// imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(greet, handleError);
