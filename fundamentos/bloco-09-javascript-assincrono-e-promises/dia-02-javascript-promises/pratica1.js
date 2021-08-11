de01831990e89b51e1f5673fb804c8510842572d
Ariane Ueti <arianeueti@gmail.com>
2021-08-11 15:44:33 -0300

    Prática usando .then e .catch

---
+++ fundamentos/bloco-09-javascript-assincrono-e-promises/dia-02-javascript-promises/pratica1.js
@@ -0,0 +1,33 @@
+const promise = new Promise((resolve, reject) => {
+  const number = Math.floor(Math.random() * 11);
+
+  if (number <= 5) {
+    return reject(console.log(`Que fracasso =( Nosso número foi ${number}`));
+  }
+  resolve(console.log(`Que sucesso =) nosso número foi ${number}`));
+});
+
+// Utilizando o .then
+const promise = new Promise((resolve, reject) => {
+  const number = Math.floor(Math.random() * 11);
+
+  if (number <= 5) {
+    return reject(console.log(`Que fracasso =( Nosso número foi ${number}`));
+  }
+  resolve(number);
+})
+.then(number => `Que sucesso =) nosso número foi ${number}`)
+.then(msg => console.log(msg));
+
+// Utilizando o .catch
+const promise = new Promise((resolve, reject) => {
+  const number = Math.floor(Math.random()* 11);
+
+  if (number <= 5) {
+    return reject(number);
+  }
+  resolve(number);
+})
+.then(number => `Que sucesso =) nosso número foi ${number}`)
+.then(msg => console.log(msg))
+.catch(number => console.log(`Que fracasso =( Nosso número foi ${number}`));
