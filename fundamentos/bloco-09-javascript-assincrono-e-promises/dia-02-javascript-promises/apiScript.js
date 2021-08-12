6b177ab7ba7d5193594f9d9491f69be25ab9372c
Ariane Ueti <arianeueti@gmail.com>
2021-08-11 21:14:53 -0300

    Exercício 1 - Jokes

---
+++ fundamentos/bloco-09-javascript-assincrono-e-promises/dia-02-javascript-promises/apiScript.js
@@ -0,0 +1,16 @@
+// apiScript.js     
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(data => document.getElementById('jokeContainer').innerText = data.joke);
+    // conosole.log(data.joke);
+};
+
+window.onload = () => fetchJoke();
--- fundamentos/bloco-09-javascript-assincrono-e-promises/dia-02-javascript-promises/pratica2.js
+++ fundamentos/bloco-09-javascript-assincrono-e-promises/dia-02-javascript-promises/pratica2.js
@@ -20,3 +20,45 @@ function sendJokeToFriend(name) {
 }
 
 sendJokeToFriend("Anna");
+
+// Usando Async
+const fetch = require('node-fetch');
+
+async function verifiedFetch(url) {
+  if (url === 'https://api.chucknorris.io/jokes/random?category=dev') {
+    return fetch(url)
+      .then((r) => r.json())
+      .then((r) => r.value);
+  }
+  throw new Error('endpoint não existe');
+}
+
+function sendJokeToFriend(name) {
+  const message = verifiedFetch('https://api.chucknorris.io/jokes/random?category=dev')
+    .then((joke) => `Oi ${name}, ouve essa: ${joke}`)
+    .catch((err) => err);
+  console.log(message);
+}
+
+sendJokeToFriend("Anna");
+
+//Usando Await
+const fetch = require('node-fetch');
+
+async function verifiedFetch(url) {
+  if (url === 'https://api.chucknorris.io/jokes/random?category=dev') {
+    return fetch(url)
+      .then((r) => r.json())
+      .then((r) => (r.value));
+  }
+  throw new Error('endpoint não existe');
+}
+
+async function sendJokeToFriend(name) {
+  const message = await verifiedFetch('https://api.chucknorris.io/jokes/random?category=dev')
+    .then((joke) => `Oi ${name}, ouve essa: ${joke}`)
+    .catch((err) => err);
+  console.log(message);
+}
+
+sendJokeToFriend("Anna");
