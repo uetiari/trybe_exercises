c2d86a7830f9d72fc24370cade5724493b320436
Ariane Ueti <arianeueti@gmail.com>
2021-08-17 18:16:10 -0300

    Instalação do Jest para resolução dos Exercícios

---
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/src/exe1.js
@@ -0,0 +1,10 @@
+// const assert = require('assert');
+
+// function sum(a, b) {
+//   if (typeof a !== 'number' || typeof b !== 'number') {
+//     throw new Error('parameters must be numbers');
+//   }
+
+//   return a + b;
+// }
+
