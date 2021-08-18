f29166e182c63bee23ee524f2836fb1d8df6325b
Ariane Ueti <arianeueti@gmail.com>
2021-08-18 15:09:11 -0300

    Exercício 01

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/test/exe1.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/test/exe1.test.js
@@ -3,7 +3,7 @@
 // Teste se o retorno de sum(0, 0) é 0
 // Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
 // Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
-const { test, expect } = require('@jest/globals');
+
 const sum = require('../src/exe1');
 
 describe('Requisito 1', () => {
