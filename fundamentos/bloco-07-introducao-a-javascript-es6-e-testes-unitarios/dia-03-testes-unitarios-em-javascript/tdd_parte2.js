24416764fe7a25077d30296c09976ea623549ecd
Ariane Ueti <arianeueti@gmail.com>
2021-08-26 18:45:23 -0300

    Exercício tdd 2

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/tdd_parte2.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/tdd_parte2.js
@@ -16,3 +16,21 @@ const output = addOne(myArray);
 assert.strictEqual(typeof addOne, 'function');
 assert.deepStrictEqual(output, expected);
 assert.deepStrictEqual(myArray, unchanged);
+
+// --------------------------------------
+const assert = require('assert');
+// escreva a função wordLengths aqui
+const wordLengths = (array) => {
+  const output = [];
+  for (let index = 0; index < array.length; index += 1) {
+    output.push(array[index].length);
+  }
+  return output;
+};
+
+const words = ['sun', 'potato', 'roundabout', 'pizza'];
+const expected = [3, 6, 10, 5];
+
+assert.strictEqual(typeof wordLengths, 'function');
+const output = wordLengths(words);
+assert.deepStrictEqual(output, expected);
