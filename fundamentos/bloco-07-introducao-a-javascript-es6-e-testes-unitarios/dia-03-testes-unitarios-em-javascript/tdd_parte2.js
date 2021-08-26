df309b8d40d2bb8fc7e374913021d7d99b6f2927
Ariane Ueti <arianeueti@gmail.com>
2021-08-26 18:46:13 -0300

    Exercício tdd 3

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/tdd_parte2.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/tdd_parte2.js
@@ -34,3 +34,21 @@ const expected = [3, 6, 10, 5];
 assert.strictEqual(typeof wordLengths, 'function');
 const output = wordLengths(words);
 assert.deepStrictEqual(output, expected);
+
+// -------------------------------
+const assert = require('assert');
+// escreva a função sumAllNumbers aqui
+const sumAllNumbers = (array) => {
+  let output = 0;
+  for (const item in array) {
+    output += array[item];
+  }
+  return output;
+};
+
+const numbers = [9, 23, 10, 3, 8];
+const expected = 53;
+const output = sumAllNumbers(numbers);
+
+assert.strictEqual(typeof sumAllNumbers, 'function');
+assert.strictEqual(output, expected);
