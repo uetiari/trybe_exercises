ea76ff87af865a71289e3219c19a59265412a2c2
Ariane Ueti <42356440+uetiari@users.noreply.github.com>
2021-08-26 17:33:13 -0300

    Exercício 4

---
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio4.js
@@ -0,0 +1,16 @@
+const longestWord = text => {
+    let wordArray = text.split(' ');
+    let maxLength = 0;
+    let result = '';
+
+    for (const word of wordArray) {
+        if (word.length > maxLength) {
+            maxLength = word.length;
+            result = word;
+        }
+    }
+
+    return result;
+}
+
+console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"));
