908658e821eeb3a652759cabd7468cb16ee580a1
Ariane Ueti <42356440+uetiari@users.noreply.github.com>
2021-08-26 17:32:16 -0300

    Exercício 3

---
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio3.js
@@ -0,0 +1,11 @@
+const factorial = number => {
+    let result = 1;
+
+    for (let index = 2; index <= number; index += 1) {
+        result *= index;
+    }
+
+    return result;
+}
+
+console.log(factorial(5));
