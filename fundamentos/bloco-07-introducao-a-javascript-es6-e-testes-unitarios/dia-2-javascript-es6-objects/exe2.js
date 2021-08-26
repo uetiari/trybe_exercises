539974c81b4951240cad3125070161b839d00404
Ariane Ueti <arianeueti@gmail.com>
2021-08-26 18:23:20 -0300

    Arrumas pastas de nome iguais

---
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/exe2.js
@@ -0,0 +1,86 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const addNewKey = (obj, key, value) => {
+  obj[key] = value;
+};
+
+addNewKey(lesson2, 'turno', 'noite');
+
+const listKeys = (obj) =>  Object.keys(obj);
+console.log(listKeys(lesson1));
+
+const sizeObj = (obj) =>  Object.keys(obj).length;
+console.log(sizeObj(lesson1));
+
+const listValues = (obj) =>  Object.values(obj);
+console.log(listValues(lesson1));
+
+console.log(allLessons);
+/*
+{
+  lesson1:
+   { materia: 'Matemática',
+     numeroEstudantes: 20,
+     professor: 'Maria Clara',
+     turno: 'manhã' },
+  lesson2:
+   { materia: 'História',
+     numeroEstudantes: 20,
+     professor: 'Carlos',
+     turno: 'noite' },
+  lesson3:
+   { materia: 'Matemática',
+     numeroEstudantes: 10,
+     professor: 'Maria Clara',
+     turno: 'noite' }
+};
+*/
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
+
+const getNumberOfStudents = (obj) => {
+  let total = 0;
+  const array = Object.keys(obj);
+  for (index in array) {
+    total += obj[array[index]].numeroEstudantes;
+  }
+  return total;
+};
+console.log(getNumberOfStudents(allLessons));
+
+const getValueByNumber = (obj,number) => Object.values(obj)[number];
+
+console.log(getValueByNumber(lesson1, 0));
+// Output: 'Matématica'
+
+
+const verifyPair = (obj, key, value) => {
+  const arr = Object.entries(obj);
+  let isEqual = false;
+  for (let index in arr) {
+    if (arr[index][0] === key && arr[index][1] === value) isEqual = true;
+  }
+  return isEqual;
+};
+
+console.log(verifyPair(lesson3, 'turno', 'noite'));
+// Output: true,
+console.log(verifyPair(lesson3, 'materia', 'Maria Clara'));
+// Output: false
