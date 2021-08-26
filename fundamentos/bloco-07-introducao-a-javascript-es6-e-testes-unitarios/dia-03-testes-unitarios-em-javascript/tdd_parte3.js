571bb3ed7d31fd7282caed7e6a993a51196acaee
Ariane Ueti <arianeueti@gmail.com>
2021-08-26 18:55:05 -0300

    Exercício tdd 4

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/tdd_parte3.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/tdd_parte3.js
@@ -68,3 +68,23 @@ const result = [32, 21];
 assert.strictEqual(typeof greaterThanTenNew, 'function');
 const output = greaterThanTenNew(parameter);
 assert.deepStrictEqual(output, result);
+
+// -----------------------------
+
+const assert = require('assert');
+
+function secondThirdSmallestNew(array) {
+    const results = []
+    array.sort((item1, item2) => {
+        return item1 - item2;
+    });
+    results.push(array[1], array[2]);
+    return results;
+};
+
+const parameter = [4, 10, 32, 9, 21, 90, 5, 11, 8, 6];
+const result = [5, 6];
+
+assert.strictEqual(typeof secondThirdSmallestNew, 'function');
+const output = secondThirdSmallestNew(parameter);
+assert.deepStrictEqual(output, result);
