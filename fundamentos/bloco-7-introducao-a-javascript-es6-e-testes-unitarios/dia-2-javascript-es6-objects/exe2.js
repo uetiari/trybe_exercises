b158a45aba06a077c98ed3737e5765b17600e0df
Ariane Ueti <arianeueti@gmail.com>
2021-08-26 18:15:03 -0300

    Parte 2 Exercício 5

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/exe2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/exe2.js
@@ -53,12 +53,5 @@ console.log(allLessons);
      turno: 'noite' }
 };
 */
-const getNumberOfStudents = (obj) => {
-  let total = 0;
-  const array = Object.keys(obj);
-  for (index in array) {
-    total += obj[array[index]].numeroEstudantes;
-  }
-  return total;
-};
-console.log(getNumberOfStudents(allLessons));
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
+
