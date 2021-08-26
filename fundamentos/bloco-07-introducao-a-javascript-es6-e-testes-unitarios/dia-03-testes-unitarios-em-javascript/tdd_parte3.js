c024a5da3028fdae76d82f74dd26b1c0e99b1ffa
Ariane Ueti <arianeueti@gmail.com>
2021-08-26 18:54:24 -0300

    Exercício tdd 3

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/tdd_parte3.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/tdd_parte3.js
@@ -47,3 +47,24 @@ const result = 'D1y2n3';
 assert.strictEqual(typeof removeVowelsNew, 'function');
 const output = removeVowelsNew(parameter);
 assert.strictEqual(output, result);
+
+// ----------------------------------------
+
+const assert = require('assert');
+
+const greaterThanTenNew = (array) => {
+  const results = [];
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] > 10) {
+      results.push(array[index]);
+    }
+  }
+  return results;
+};
+
+const parameter = [4, 10, 32, 9, 21];
+const result = [32, 21];
+
+assert.strictEqual(typeof greaterThanTenNew, 'function');
+const output = greaterThanTenNew(parameter);
+assert.deepStrictEqual(output, result);
