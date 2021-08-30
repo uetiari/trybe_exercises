0e5ec3f8260f8544d7f67a5b3db4ca46e05b76fe
Ariane Ueti <42356440+uetiari@users.noreply.github.com>
2021-08-30 20:48:55 -0300

    Prática 6

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/pratica6.js
@@ -0,0 +1,13 @@
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  return arr.every((person) => person.age >= minimumAge); // cada item do array, se a idade for maior ou igual ao minimumAge
+}
+
+console.log(verifyAges(people, 18));
