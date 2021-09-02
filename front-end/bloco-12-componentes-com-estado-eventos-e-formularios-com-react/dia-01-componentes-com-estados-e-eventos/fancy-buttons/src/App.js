c18e3067213fcddc15cccc2dbb6a8210f625f4b4
Ariane Ueti <arianeueti@gmail.com>
2021-09-02 18:00:44 -0300

    Adiciona comentários para fixação de aprendizado

--- front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estados-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estados-e-eventos/fancy-buttons/src/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from 'react'; // importa o React
+import './App.css'; // importa o estilo
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      clickNumbers: 0
+class App extends React.Component { // tranforma a função em uma classe
+  constructor() { // chama constructor para colocar as coisas que irão compor o App
+    super() // sobreescreve o padrão do React e não perder características do Pai
+    this.state = { // define um estado padrão
+      clickNumbers: 0 // começa com zero clicks
     }
-    this.handleClick = this.handleClick.bind(this);
+    this.handleClick = this.handleClick.bind(this); // bind vai fazer com que o .this tenha o mesmo valor do .this da classe
   }
   
   handleClick = () => {
-    this.setState((stateBefore, _props) => ({
+    this.setState((stateBefore, _props) => ({ // primeiro valor é estado anterior, e segundo são as props que com "_" não é necessário no momento
       clickNumbers: stateBefore.clickNumbers + 1
-    }))
+    })) // chamando a callback no setStage dá pra ver a atualização dos clickNumbers
   }
+  // caso não queria usar estado anterior pode ser passado um obj
   
   render() {
     return <button onClick={ this.handleClick }>{ this.state.clickNumbers }</button>
