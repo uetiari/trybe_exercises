0a1c4217b77f08f379aab0f89ff1ef0bb86ed91f
Ariane Ueti <arianeueti@gmail.com>
2021-09-02 15:41:02 -0300

    Prática 3 e 4

--- front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estados-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estados-e-eventos/fancy-buttons/src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import './App.css';
 
-function handleClick() {
-  console.log('Clicou, miserávi!')
-}
-
-function handleClick2() {
-  console.log('Clicou!')
-}
-
-function handleClick3() {
-  console.log('Clicou 3!')
-}
-
 class App extends React.Component {
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick() {
+    console.log('Clicou, miserávi!')
+  }
+  
   render(){
     return (
       <section className="buttons">
-        <button onClick={handleClick}>Aperte aqui!</button>
-        <button onClick={handleClick2}>Aperte aqui!</button>
-        <button onClick={handleClick3}>Aperte aqui!</button>
+        <button onClick={ this.handleClick }>Aperte aqui!</button>
       </section>
     );
   }
