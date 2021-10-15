2e7a75830f15c6f58808a2f2ebfbefa87f167a0e
uetiari <arianeueti@gmail.com>
2021-10-15 17:44:28 -0300

    Prática Primeiro Component com React

---
+++ front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/hello-world-react/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Component from './Component';
+
+function App() {
+  return (
+    <Component />
+  );
+}
+
+export default App;
---
+++ front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/hello-world-react/src/Component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Component extends React.Component {
+  render() {
+    return (
+      <section>
+        <h1>Ariane Ueti</h1>
+        <p>
+          Meu interesse em Desenvolvimento veio após acompanhar inúmeras vezes meu marido em eventos de Desenvolvimento de Games e que me chamou atenção de não se ver muitas mulheres atuando na área. 
+        </p>
+      </section>
+    )
+  }
+}
+
+export default Component;
